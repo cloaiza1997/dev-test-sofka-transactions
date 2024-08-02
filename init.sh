7ca98b247aa9f168ddf3c7b9589918363dd67709
Cristian Loaiza <kla-1997@hotmail.com>
2024-08-01 19:21:39 -0500

    Add env var to IP

--- init.sh
+++ init.sh
@@ -16,7 +16,6 @@ docker exec rabbitmq rabbitmq-plugins enable rabbitmq_amqp1_0
 echo "*** rabbitmq restart ***"
 
 docker stop rabbitmq
-
 docker start rabbitmq
 
 echo "*** rabbitmq is ready to use ***"
@@ -24,10 +23,9 @@ echo "*** rabbitmq is ready to use ***"
 # Api
 echo "*** quarkus init ***"
 
+./mvnw clean
 ./mvnw package
-
 docker build -f src/main/docker/Dockerfile.jvm -t quarkus/dev-test-sofka-transactions .
-
 docker run -d --name transactions_api -p 8080:8080 quarkus/dev-test-sofka-transactions
 
 echo "*** quarkus end ***"
